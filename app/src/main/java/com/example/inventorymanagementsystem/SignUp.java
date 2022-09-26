5f3dc08343a4b14409a386115f9a266223ccf408
Vishakha Patariya <95237978+123vf@users.noreply.github.com>
2022-09-26 22:47:10 +0530

    App

--- app/src/main/java/com/example/inventorymanagementsystem/SignUp.java
+++ app/src/main/java/com/example/inventorymanagementsystem/SignUp.java
@@ -40,6 +40,7 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().hide();
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
